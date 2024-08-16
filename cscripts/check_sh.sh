9308db2875252484947ac6d4dfb7d44d05f5c286
Bruno Mazzotti <bruno.mazzotti@amd.com>
2024-08-16 20:35:52 +0000

    Use ShellCheck resource file

--- cscripts/check_sh.sh
+++ cscripts/check_sh.sh
@@ -2,9 +2,4 @@
 
 script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-shellcheck \
-    --shell=bash \
-    --check-sourced \
-    --source-path="${script_dir}" \
-    "${script_dir}"/*.sh \
-    "${script_dir}/cscripts"/*.sh
+shellcheck "${script_dir}"/*.sh
