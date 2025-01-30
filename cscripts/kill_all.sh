758e1c3217f02b756b82008f799e585bf1d291ce
Bruno Mazzotti <bruno.mazzotti@amd.com>
2025-01-30 13:35:05 +0000

    Fix bug in kill_all.sh script

--- cscripts/kill_all.sh
+++ cscripts/kill_all.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
+# Avoid suicide, fratricide and killing init.
+do_not_kill_pid_list=("${$}" "${PPID}" 1)
+
 echo "Killing all [${1}] processes..."
 
 for pid in $(pgrep "${1}"); do
-    # This `if` statement is to avoid suicide.
-    if (( "${pid}" != "${$}" )); then
+    kill_pid=1
+    for do_not_kill_pid in "${do_not_kill_pid_list[@]}"; do
+	if [[ "${pid}" = "${do_not_kill_pid}" ]]; then
+	    kill_pid=0
+	    break
+	fi
+    done
+    if [[ "${kill_pid}" = 1 ]]; then
 	echo "Killing PID [${pid}]..."
 	kill -9 "${pid}" &> /dev/null &
     fi
