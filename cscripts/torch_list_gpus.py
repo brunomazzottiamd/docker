e0241137ac865266175770d4ffc7f4a47d6f9a09
Bruno Mazzotti <bruno.mazzotti@amd.com>
2025-03-03 14:15:49 +0000

    Handle runtime error in `torch_list_gpus.py`

--- cscripts/torch_list_gpus.py
+++ cscripts/torch_list_gpus.py
@@ -12,4 +12,7 @@ else:
     gpu_count: int = torch.cuda.device_count()
     print(f"Number of available GPUs = {gpu_count}")
     for gpu_index in range(gpu_count):
-        print(f"GPU {gpu_index} = {torch.cuda.get_device_name(gpu_index)}")
+        try:
+            print(f"GPU {gpu_index} = {torch.cuda.get_device_name(gpu_index)}")
+        except RuntimeError as e:
+            print(f"GPU {gpu_index} = runtime error << {e} >>")
