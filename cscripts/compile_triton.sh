373002dbc0e6b7153f839b6ab67f653aa6d8becc
Bruno Mazzotti <bruno.mazzotti@amd.com>
2025-04-24 13:19:41 +0000

    Fix Triton installation script to use new install instructions

--- cscripts/compile_triton.sh
+++ cscripts/compile_triton.sh
@@ -4,6 +4,6 @@ export PIP_ROOT_USER_ACTION=ignore
 export TRITON_BUILD_WITH_CCACHE=true
 
 pip uninstall --yes triton && \
-cd /triton_dev/triton/python && \
-# FIXME: `--editable` option of `pip install` is causing trouble to `import triton`.
-pip install --verbose --no-build-isolation .
+cd /triton_dev/triton && \
+pip install --requirement python/requirements.txt && \
+pip install --verbose --no-build-isolation --editable .
