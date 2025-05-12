02118247bdcdd1176e8f3bf43d48ae5f50795e18
Bruno Mazzotti <bruno.mazzotti@amd.com>
2025-05-12 16:54:39 +0000

    Improve reporting of `count_triton_cache.sh` script

--- cscripts/count_triton_cache.sh
+++ cscripts/count_triton_cache.sh
@@ -4,7 +4,15 @@ triton_cache_dir="${HOME}/.triton/cache"
 
 if [ -d "${triton_cache_dir}" ]; then
     count=$(find "${triton_cache_dir}" -type f -name '*.amdgcn' | wc --lines)
-    echo "There are ${count} compiled kernels in Triton cache."
+    if [ "${count}" -eq 0 ]; then
+	echo 'Triton cache is empty.'
+    elif [ "${count}" -eq 1 ]; then
+	echo 'There is 1 compiled kernel in Triton cache.'
+    elif [ "${count}" -gt 1 ]; then
+	echo "There are ${count} compiled kernels in Triton cache."
+    else
+	echo 'The number of compiled Triton kernels is negative, this is unexpected.'
+    fi
 else
     echo "Triton cache directory doesn't exist."
 fi
