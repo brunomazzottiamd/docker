93b72f2c28c6d52b4889260f22b845f863d636c6
Bruno Mazzotti <bruno.mazzotti@amd.com>
2025-05-12 22:07:44 +0000

    Report Triton cache size in monitoring script

--- cscripts/count_triton_cache.sh
+++ cscripts/count_triton_cache.sh
@@ -4,12 +4,13 @@ triton_cache_dir="${HOME}/.triton/cache"
 
 if [ -d "${triton_cache_dir}" ]; then
     count=$(find "${triton_cache_dir}" -type f -name '*.amdgcn' | wc --lines)
+    size=$(du --summarize --human-readable "${triton_cache_dir}" | cut --fields=1)
     if [ "${count}" -eq 0 ]; then
-	echo 'Triton cache is empty.'
+	echo "Triton cache is empty (${size})."
     elif [ "${count}" -eq 1 ]; then
-	echo 'There is 1 compiled kernel in Triton cache.'
+	echo "There is 1 compiled kernel in Triton cache (${size})."
     elif [ "${count}" -gt 1 ]; then
-	echo "There are ${count} compiled kernels in Triton cache."
+	echo "There are ${count} compiled kernels in Triton cache (${size})."
     else
 	echo 'The number of compiled Triton kernels is negative, this is unexpected.'
     fi
