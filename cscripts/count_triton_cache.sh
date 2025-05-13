c1f238a30e4c4be1397d7f18e2e1844489b665f5
Bruno Mazzotti <bruno.mazzotti@amd.com>
2025-05-13 14:42:11 +0000

    Count TTIR, TTGIR, LLIR, AMDGCN and HSACO files in Triton cache

--- cscripts/count_triton_cache.sh
+++ cscripts/count_triton_cache.sh
@@ -3,17 +3,26 @@
 triton_cache_dir="${HOME}/.triton/cache"
 
 if [ -d "${triton_cache_dir}" ]; then
-    count=$(find "${triton_cache_dir}" -type f -name '*.amdgcn' | wc --lines)
+    ttir_count=$(find "${triton_cache_dir}" -type f -name '*.ttir' | wc --lines)
+    ttgir_count=$(find "${triton_cache_dir}" -type f -name '*.ttgir' | wc --lines)
+    llir_count=$(find "${triton_cache_dir}" -type f -name '*.llir' | wc --lines)
+    amdgcn_count=$(find "${triton_cache_dir}" -type f -name '*.amdgcn' | wc --lines)
+    hsaco_count=$(find "${triton_cache_dir}" -type f -name '*.hsaco' | wc --lines)
     size=$(du --summarize --human-readable "${triton_cache_dir}" | cut --fields=1)
-    if [ "${count}" -eq 0 ]; then
-	echo "Triton cache is empty (${size})."
-    elif [ "${count}" -eq 1 ]; then
-	echo "There is 1 compiled kernel in Triton cache (${size})."
-    elif [ "${count}" -gt 1 ]; then
-	echo "There are ${count} compiled kernels in Triton cache (${size})."
+
+    if [ "${hsaco_count}" -eq 0 ]; then
+	echo 'There is no fully compiled kernel in Triton cache.'
+    elif [ "${hsaco_count}" -eq 1 ]; then
+	echo 'There is 1 fully compiled kernel in Triton cache.'
+    elif [ "${hsaco_count}" -gt 1 ]; then
+	echo "There are ${hsaco_count} fully compiled kernels in Triton cache."
     else
-	echo 'The number of compiled Triton kernels is negative, this is unexpected.'
+	echo 'The number of compiled Triton kernels is negative, this is unexpected!'
     fi
+
+    msg="(${ttir_count} TTIR, ${ttgir_count} TTGIR, ${llir_count} LLIR, ${amdgcn_count} AMDGCN, ${hsaco_count} HSACO, ${size})"
+    echo "${msg}"
+
 else
     echo "Triton cache directory doesn't exist."
 fi
